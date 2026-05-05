d3993a0f9ad94c3211b55210d0dcf3bcdcaf163c
sirajul777 <thony.raj880@gmail.com>
2026-05-05 16:19:51 +0800

    all fix mobile

--- src/mobile-api/mobile-api.module.ts
+++ src/mobile-api/mobile-api.module.ts
@@ -4,9 +4,11 @@ import { BotResellerModule } from "../reseller-bot/bot-reseller.module";
 import { BillingModule } from "../billing/billing.module";
 import { MikrotikModule } from "../mikrotik/mikrotik.module";
 import { ConfigModule } from "../config/config.module";
+import { MobileAuthService } from "./mobile-api.service";
 
 @Module({
   imports: [BotResellerModule, BillingModule, MikrotikModule, ConfigModule],
+  providers: [MobileAuthService],
   controllers: [MobileApiController]
 })
 export class MobileApiModule {}
